3535f952b3c1f8187dcef4f71a343f70c343b812
Alejandro Quiroz <alejandroq1113@gmail.com>
2023-01-09 09:22:19 -0500

    Event based communication with imported federated modules set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'shell_1';
+
+  constructor() {
+    window.addEventListener('ready', (readyEvent) => {
+      console.log(readyEvent)
+      Promise.resolve()
+        .then(() => {
+          window.dispatchEvent(new CustomEvent('addOriginData', {
+            detail: {
+              origin: 'shell_1',
+              form: {
+                title: 'Shell 1 Calling Bitch!',
+                from: 'MDE',
+                to: 'LHR',
+                user: {
+                  userName: 'hiddenUserName',
+                  token: '1234567890xaBcD,987654321;neededKeyOrInfo'
+                }
+              }
+            }
+          }))
+        })
+    })
+  }
 }
